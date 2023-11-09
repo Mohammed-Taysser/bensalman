d8afa21de0d9ce615f9b65c11ad7bedff0ef747f
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-09 13:56:01 +0200

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,19 @@
+import { Col, Image, Row } from 'antd';
+import image404 from '../assets/images/background/404.svg';
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <div>
+      <Row
+        className='min-h-screen justify-center md:justify-around menu-page '
+        align='middle'
+      >
+        <Col xs={20} md={10}>
+          <Image preview={false} src={image404} />
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
