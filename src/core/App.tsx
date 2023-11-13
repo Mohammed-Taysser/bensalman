db44c1671dcf288287b2844c04bedecd62c38d2e
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-13 16:25:12 +0200

    add redux  store to app

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,7 +1,9 @@
 import { ConfigProvider, theme } from 'antd';
 import { Suspense } from 'react';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import SuspenseLoading from '../components/SuspenseLoading';
+import store from '../redux/store';
 import routes from './routes';
 
 import 'swiper/scss';
@@ -10,36 +12,41 @@ import '../assets/scss/styles.scss';
 
 function App() {
   return (
-    <ConfigProvider
-      theme={{
-        algorithm: theme.darkAlgorithm,
-        token: {
-          colorPrimary: '#dda300',
-          fontFamily: `'Lato', sans-serif`,
-        },
-        components: {
-          Typography: {
+    <Provider store={store}>
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+          token: {
+            colorPrimary: '#dda300',
             fontFamily: `'Lato', sans-serif`,
           },
-          Button: {
-            colorPrimary: '#cfa670',
-            primaryColor: '#000',
+          components: {
+            Typography: {
+              fontFamily: `'Lato', sans-serif`,
+            },
+            Button: {
+              colorPrimary: '#cfa670',
+              primaryColor: '#000',
+            },
+            Badge: {
+              colorBgContainer: '#fff',
+              textFontWeight: 'bold',
+              textFontSize: 14,
+            },
+            InputNumber: {
+              handleVisible: true,
+            },
           },
-          Badge: {
-            colorBgContainer: '#fff',
-            textFontWeight: 'bold',
-            textFontSize: 14,
-          },
-          InputNumber: {
-            handleVisible: true,
-          },
-        },
-      }}
-    >
-      <Suspense fallback={<SuspenseLoading />}>
-        <RouterProvider router={routes} fallbackElement={<SuspenseLoading />} />
-      </Suspense>
-    </ConfigProvider>
+        }}
+      >
+        <Suspense fallback={<SuspenseLoading />}>
+          <RouterProvider
+            router={routes}
+            fallbackElement={<SuspenseLoading />}
+          />
+        </Suspense>
+      </ConfigProvider>
+    </Provider>
   );
 }
 
