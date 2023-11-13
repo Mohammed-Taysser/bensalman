36e0598a78537827f492b7848791819323cbbd39
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-13 16:26:28 +0200

    create hook for redux usage

---
+++ src/hooks/useRedux.ts
@@ -0,0 +1,11 @@
+import { useDispatch, useSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
+import type { RootState, AppDispatch } from '../redux/store';
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+const useAppDispatch: () => AppDispatch = useDispatch;
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+const selectAuth = (state: RootState) => state.auth;
+
+export { useAppDispatch, useAppSelector, selectAuth };
