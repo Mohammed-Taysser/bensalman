1f142fc745af9645945ba3e060446490a2bdf28d
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-15 12:36:18 +0200

    integrate menu page with api

--- src/components/menu/ProductModal.tsx
+++ src/components/menu/ProductModal.tsx
@@ -1,14 +1,15 @@
 import { Button, Col, Image, InputNumber, Modal, Row, Typography } from 'antd';
 import { useEffect, useState } from 'react';
+import { SERVER_URL } from '../../core/api';
 
 function ProductModal(props: Readonly<MenuProductModalProps>) {
   const { isOpen, onClose, product } = props;
 
-  const [qty, setQty] = useState(product.qty);
+  const [qty, setQty] = useState(product.cart_qty);
 
   useEffect(() => {
     if (isOpen) {
-      setQty(product.qty);
+      setQty(product.cart_qty);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isOpen]);
@@ -31,17 +32,16 @@ function ProductModal(props: Readonly<MenuProductModalProps>) {
     >
       <Row gutter={{ xs: 10, md: 20 }}>
         <Col xs={24} md={10}>
-          <Image
-            src='https://wgl-demo.net/benoit/wp-content/uploads/2021/03/bg-column_02.jpg'
-            preview={false}
-          />
+          <Image src={SERVER_URL + product.image} preview={false} />
         </Col>
 
         <Col xs={24} md={14}>
-          <Typography.Title level={4}>{product.title}</Typography.Title>
-          <Typography.Title level={5}>${product.price}</Typography.Title>
+          <Typography.Title level={4}>{product.item_name}</Typography.Title>
+          <Typography.Title level={5}>
+            ${product.standard_rate}
+          </Typography.Title>
           <Row className='mb-3' justify='start'>
-            {product.qty > 0 ? (
+            {product.cart_qty > 0 ? (
               <InputNumber
                 controls
                 min={1}
