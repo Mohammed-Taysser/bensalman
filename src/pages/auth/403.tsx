74e99eacbc7e8311d30c0ec0a0716d3df18394b4
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-15 14:07:16 +0200

    update 403 page

--- src/pages/auth/403.tsx
+++ src/pages/auth/403.tsx
@@ -1,7 +1,36 @@
-import { Typography } from 'antd';
+import { Col, Image, Row, Typography } from 'antd';
+import image403 from '../../assets/images/background/403.svg';
+import columnBG from '../../assets/images/background/bg-column.png';
+import welcomeBG from '../../assets/images/background/welcome.jpeg';
+import Base from '../../layouts/Base';
 
 function NotAuthorized() {
-  return <Typography.Title>NotAuthorized</Typography.Title>;
+  return (
+    <Base bg={welcomeBG}>
+      <Row
+        className='min-h-screen justify-center md:justify-around menu-page '
+        align='middle'
+      >
+        <Col xs={22} md={18}>
+          <Row
+            gutter={{ xs: 0, md: 20 }}
+            align='middle'
+            className='menu-wrapper'
+            style={{
+              backgroundImage: `url('${columnBG}')`,
+            }}
+          >
+            <Col xs={24} className='text-center my-16 '>
+              <Image preview={false} src={image403} width={300} />
+              <Typography.Title className='!text-aurora' level={3}>
+                You't Authorized To See This Page
+              </Typography.Title>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Base>
+  );
 }
 
 export default NotAuthorized;
