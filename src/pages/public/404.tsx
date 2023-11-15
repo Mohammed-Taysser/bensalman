f2443e5892c1068f5f54b439a9b0e4160593ae91
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-15 14:08:19 +0200

    update 404 page

--- src/pages/public/404.tsx
+++ src/pages/public/404.tsx
@@ -1,18 +1,45 @@
-import { Col, Image, Row } from 'antd';
+import { Button, Col, Image, Row, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
 import image404 from '../../assets/images/background/404.svg';
+import columnBG from '../../assets/images/background/bg-column.png';
+import welcomeBG from '../../assets/images/background/welcome.jpeg';
+import Base from '../../layouts/Base';
 
 function PageNotFound() {
+  const navigateTo = useNavigate();
+
+  const onClick = () => {
+    navigateTo('/');
+  };
+
   return (
-    <div>
+    <Base bg={welcomeBG} noNavbar>
       <Row
         className='min-h-screen justify-center md:justify-around menu-page '
         align='middle'
       >
-        <Col xs={20} md={10}>
-          <Image preview={false} src={image404} />
+        <Col xs={22} md={18}>
+          <Row
+            gutter={{ xs: 0, md: 20 }}
+            align='middle'
+            className='menu-wrapper'
+            style={{
+              backgroundImage: `url('${columnBG}')`,
+            }}
+          >
+            <Col xs={24} className='text-center my-16 '>
+              <Image preview={false} src={image404} width={300} />
+
+              <Typography.Title className='!text-aurora' level={4}>
+                Sorry, the page you're looking for not exist or might be delete
+              </Typography.Title>
+
+              <Button onClick={onClick}>Take Me Home</Button>
+            </Col>
+          </Row>
         </Col>
       </Row>
-    </div>
+    </Base>
   );
 }
 
