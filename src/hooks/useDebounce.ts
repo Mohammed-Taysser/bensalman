9f1eeb39ee40f51a58a3b6b7b796ff6611c7be53
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-16 15:42:23 +0200

    create `useDebounce` hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+function useDebounce(value: number, delay: number = 500) {
+  const [debouncedValue, setDebouncedValue] = useState<number>(0);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [value, delay]);
+
+  return { debouncedValue, setDebouncedValue };
+}
+
+export default useDebounce;
