4a48bf7a47980498468293c4de24c56cc19b7e84
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-16 16:38:16 +0200

    refactor cart item component

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,21 +1,69 @@
-import { Col, Image, InputNumber, Row, Typography } from 'antd';
-import { useState } from 'react';
+import { Col, Image, InputNumber, Row, Spin, Typography, message } from 'antd';
+import { useEffect, useRef, useState } from 'react';
+import { API } from '../../core/api';
+import { getErrorMessage, getImageUrl } from '../../helper';
+import useDebounce from '../../hooks/useDebounce';
+import { useAppDispatch } from '../../hooks/useRedux';
+import { setUserStatus } from '../../redux/slices/status.slice';
 
 function CartItem(props: Readonly<{ product: Product }>) {
   const { product } = props;
+  const dispatch = useAppDispatch();
+
+  const [messageApi, contextHolder] = message.useMessage();
+  const fistInit = useRef(true);
 
   const [qty, setQty] = useState(product.cart_qty);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { debouncedValue } = useDebounce(qty);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fistInit.current = false;
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (!fistInit.current) {
+      modifyQty();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue]);
+
+  const modifyQty = async () => {
+    setIsLoading(true);
+
+    API.modifyCartQuantity({ item: product.item_name, qty: debouncedValue })
+      .then((response) => {
+        dispatch(setUserStatus(response.data.data));
+      })
+      .catch((error) => {
+        messageApi.open({
+          type: 'error',
+          content: getErrorMessage(error),
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   const onQtyChange = (value: number | null) => {
-    if (value) {
+    if (value !== null && !isNaN(value)) {
       setQty(value);
     }
   };
 
+  if (!product) {
+    return null;
+  }
+
   return (
     <Row gutter={{ xs: 10, md: 20 }}>
+      {contextHolder}
       <Col>
-        <Image src={product.image} preview={false} width={90} />
+        <Image src={getImageUrl(product.image)} preview={false} width={90} />
       </Col>
 
       <Col>
@@ -24,11 +72,13 @@ function CartItem(props: Readonly<{ product: Product }>) {
         </Typography.Title>
 
         <Typography.Title level={5} className='!my-2'>
-          ${product.standard_rate}
+          {product.total} ج.م
         </Typography.Title>
 
         <Row className='mb-3' justify='start'>
-          <InputNumber controls value={qty} onChange={onQtyChange} />
+          <Spin spinning={isLoading}>
+            <InputNumber controls value={qty} min={0} onChange={onQtyChange} />
+          </Spin>
         </Row>
       </Col>
     </Row>
