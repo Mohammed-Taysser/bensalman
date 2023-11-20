f1517ece5afe8b6c24d94a84463db8e9ebc834a7
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-20 12:46:13 +0200

    add cart select for useRedux hook

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
-import type { RootState, AppDispatch } from '../redux/store';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '../redux/store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 const useAppDispatch: () => AppDispatch = useDispatch;
@@ -8,5 +8,6 @@ const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 const selectAuth = (state: RootState) => state.auth;
 const selectStatus = (state: RootState) => state.status;
+const selectCart = (state: RootState) => state.cart;
 
-export { useAppDispatch, useAppSelector, selectAuth, selectStatus };
+export { selectAuth, selectCart, selectStatus, useAppDispatch, useAppSelector };
