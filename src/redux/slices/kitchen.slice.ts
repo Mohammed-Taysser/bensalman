31d5e8e6c746f9878a1b34f6c31d9a0cbc66af47
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-11-27 13:55:16 +0200

    update kitchen slice with api integration

--- src/redux/slices/kitchen.slice.ts
+++ src/redux/slices/kitchen.slice.ts
@@ -1,4 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { AxiosError } from 'axios';
+import API from '../../core/api';
+import { getErrorMessage } from '../../helper';
+
+const getKitchenInfo = createAsyncThunk(
+  'kitchen/get-info',
+  async (body: { status: string | null; shift: string | null }, thunkApi) => {
+    try {
+      const response = await API.getKitchenInfo(body);
+      return response.data;
+    } catch (err) {
+      const error = err as AxiosError<ResponseError>;
+
+      return thunkApi.rejectWithValue(getErrorMessage(error));
+    }
+  }
+);
 
 const initialState: RequestState<KitchenStatusSlice> = {
   data: {
@@ -9,10 +26,13 @@ const initialState: RequestState<KitchenStatusSlice> = {
       completed: 0,
       onTable: 0,
       total: 0,
+      Prepare: 0,
     },
     options: {
-      shift: [],
+      shifts: [],
       status: [],
+      selectedStatus: '',
+      selectedShift: '',
     },
   },
   status: 'idle',
@@ -22,7 +42,77 @@ const initialState: RequestState<KitchenStatusSlice> = {
 const kitchenSlice = createSlice({
   name: 'kitchen',
   initialState,
-  reducers: {},
+  reducers: {
+    // setKitchenProduct: (state, action: KitchenProductPayload) => {
+    //   state.data.products = action.payload.products;
+    //   state.data.options.status = action.payload.options.status;
+    //   state.data.options.shifts = action.payload.options.shifts;
+    //   // FIX:selectedShift & selectedStatus not right from api
+    //   state.data.options.selectedShift =
+    //     action.payload.options.selectedShift ??
+    //     action.payload.options.shifts[0].value;
+    //   state.data.options.selectedStatus =
+    //     action.payload.options.selectedStatus ??
+    //     action.payload.options.status[0].value;
+    // },
+    setKitchenStatusCard: (state, action: { payload: KitchenCardStatus }) => {
+      state.data.status = action.payload;
+    },
+
+    setKitchenOrders: (state, action: { payload: KitchenOrder[] }) => {
+      state.data.orders = action.payload;
+    },
+    setSelectedStatusDropdown: (state, action) => {
+      state.data.options.selectedStatus = action.payload;
+    },
+    setSelectedShiftDropdown: (state, action) => {
+      state.data.options.selectedShift = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getKitchenInfo.pending, (state) => {
+        state.status = 'loading';
+        state.error = '';
+      })
+      .addCase(getKitchenInfo.fulfilled, (state, action) => {
+        const selectedShift =
+          state.data.options.selectedShift ??
+          action.payload.main_status.shifts[0].value;
+
+        const selectedStatus =
+          state.data.options.selectedStatus ??
+          action.payload.main_status.status[0].value;
+
+        const payload = {
+          products: action.payload.KitchenProduct,
+          options: {
+            status: action.payload.main_status.status,
+            shifts: action.payload.main_status.shifts,
+            selectedStatus,
+            selectedShift,
+          },
+          orders: action.payload.data,
+          status: action.payload.status,
+        };
+
+        state.status = 'succeeded';
+        state.data = payload;
+      })
+      .addCase(getKitchenInfo.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload;
+      });
+  },
 });
 
 export default kitchenSlice.reducer;
+export const {
+  // setKitchenProduct,
+  setKitchenStatusCard,
+  setKitchenOrders,
+
+  setSelectedStatusDropdown,
+  setSelectedShiftDropdown,
+} = kitchenSlice.actions;
+export { getKitchenInfo };
