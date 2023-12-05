6c6b937a127c673efb15145429f5087a1731287c
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-12-05 10:10:54 +0200

    update login page test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,28 +1,21 @@
 describe('Login Flow', () => {
   beforeEach(() => {
     // Visit the login page
-    cy.visit('/login', {
-      onBeforeLoad: () => {
-        // alias a DOM element for later use
-        cy.get('#email').as('email');
-        cy.get('button[type="submit"]').as('submitBtn');
-        cy.get('#password').as('password');
-      },
-    });
+    cy.visit('/login');
   });
 
   it('should login successfully', () => {
     // Enter email and password
     cy.fixture('login').then((info: LoginInfoJson) => {
-      cy.get('@email').type(info.email);
-      cy.get('@password').type(info.password);
+      cy.getByData('email').type(info.email);
+      cy.getByData('password').type(info.password);
     });
 
     // Submit the login form
-    cy.get('@submitBtn').click();
+    cy.getByData('button').click();
 
     // Assert that the user is logged in
-    cy.location().should('include', '/');
+    cy.location('pathname').should('eq', '/');
   });
 
   it('should show email not valid on missing email & password', () => {
@@ -30,61 +23,66 @@ describe('Login Flow', () => {
     cy.visit('/login');
 
     // Submit the login form
-    cy.get('@submitBtn').click();
+    cy.getByData('button').click();
 
     // Assert that the user is logged in
-    cy.get('#password_help .ant-form-item-explain-error').should('be.visible');
-    cy.get('#email_help .ant-form-item-explain-error').should('be.visible');
+    cy.get('#password_help > .ant-form-item-explain-error').should(
+      'be.visible'
+    );
+    cy.get('#email_help > .ant-form-item-explain-error').should('be.visible');
   });
 
   it('should show email not valid on bad email', () => {
     // Enter email and password
     cy.fixture('login').then((info: LoginInfoJson) => {
-      cy.get('@email').type(info['bad-email']);
-      cy.get('@password').type(info.password);
+      cy.getByData('email').type(info['bad-email']);
+      cy.getByData('password').type(info.password);
     });
 
     // Submit the login form
-    cy.get('@submitBtn').click();
+    cy.getByData('button').click();
 
     // Assert that the user is logged in
-    cy.get('#email_help .ant-form-item-explain-error').should('be.visible');
+    cy.get('#email_help > .ant-form-item-explain-error').should('be.visible');
   });
 
   it('should show email not valid on missing email', () => {
     // Enter password
     cy.fixture('login').then((info: LoginInfoJson) => {
-      cy.get('@password').type(info.password);
+      cy.getByData('password').type(info.password);
     });
 
     // Submit the login form
-    cy.get('@submitBtn').click();
+    cy.getByData('button').click();
 
     // Assert that the user is logged in
-    cy.get('#email_help .ant-form-item-explain-error').should('be.visible');
+    cy.get('#email_help > .ant-form-item-explain-error').should('be.visible');
   });
 
   it('should show password not valid on missing password', () => {
     // Enter email
     cy.fixture('login').then((info: LoginInfoJson) => {
-      cy.get('@email').type(info.email);
+      cy.getByData('email').type(info.email);
     });
+
     // Submit the login form
-    cy.get('@submitBtn').click();
+    cy.getByData('button').click();
 
     // Assert that the user is logged in
-    cy.get('#password_help .ant-form-item-explain-error').should('be.visible');
+    cy.get('#password_help > .ant-form-item-explain-error').should(
+      'be.visible'
+    );
   });
 
   it('should show email or password not valid on wrong auth', () => {
     // Enter email and password
     cy.fixture('login').then((info: LoginInfoJson) => {
-      cy.get('@email').type(info['non-exist-email']);
-      cy.get('@password').type(info.password);
+      cy.getByData('email').type(info['non-exist-email']);
+      cy.getByData('password').type(info.password);
     });
 
     // Submit the login form
-    cy.get('@submitBtn').click();
+    cy.getByData('button').click();
 
     // Show error message
     cy.get(
